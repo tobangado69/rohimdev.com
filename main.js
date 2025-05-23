c5f6fefafc5acbb004f716d3912236122351b877
Abdul Rohim <arohim@students.hacktiv8.ac.id>
2025-05-23 23:11:36 +0700

    done at home page

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".title",{
+    strings:["Frontend Developer","Streamer", "Web Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
